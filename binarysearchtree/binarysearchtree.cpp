cf10c86f4e1af2d6c9c72d190113fea8b6db9a42
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-06-30 19:28:12 +0700

    untuk mencari node yang akan menjadi iduk dari node yang akan dimasukan

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -30,5 +30,13 @@ public:
 		newNode->leftchild = NULL; //Make the left child of the new node point to NULL
 		newNode->rightchild = NULL; //Make the right child of the new node point to NULL
 
-		
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL) { //If the parent is NULL (Tree is empty)
+			ROOT = newNode; //Mark the new node as ROOT
+			return; //Exit
+		}
+
 		
